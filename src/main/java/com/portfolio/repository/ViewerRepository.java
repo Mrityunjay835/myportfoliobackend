b07b3c4057151b4b74ad94c066207c0cbeda9f56
Mrityunjay Kumar <mrityunjaykumar835@gmail.com>
2025-08-20 15:58:54 +0530

    Update ViewerRepository.java

--- src/main/java/com/portfolio/repository/ViewerRepository.java
+++ src/main/java/com/portfolio/repository/ViewerRepository.java
@@ -2,7 +2,6 @@ package com.portfolio.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.portfolio.entity.Viewer;
-
 public interface ViewerRepository extends JpaRepository<Viewer , Integer>{
 
 }
