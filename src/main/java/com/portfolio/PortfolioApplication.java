0ed4102f2ab7ef96b42c64a5a8c212e43c6cc29b
Mrityunjay Kumar <mrityunjaykumar835@gmail.com>
2026-01-24 01:20:02 +0530

    Update PortfolioApplication.java

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 public class PortfolioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
+		SpringApplication.SDF(PortfolioApplication.class, args);
 	}
 	
 	@Bean
